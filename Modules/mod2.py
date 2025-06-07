b0cdeddbe61b0326988f9311b46d14d6e4f61bba
Andrey Chizhov <Achizhovjob@yandex.ru>
2025-06-07 20:56:29 +0400

    all

--- Modules/mod2.py
+++
@@ -1,8 +0,0 @@
-import mod1
-from mod1 import x
-
-print(id(x))
-
-mod1.y += ["some"]
-
-mod1.x += 1
--- Modules/mod3.py
+++
@@ -1,9 +0,0 @@
-import mod2
-
-from mod1 import y, x
-
-print(id(y))
-print(id(x))
-
-print(id(mod2.mod1.x))
-print(id(mod2.mod1.y))
